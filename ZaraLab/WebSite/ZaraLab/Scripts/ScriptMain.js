65a3266cfca07b88db4cec5da533919e4d0e436a
msantosg <36162347+msantosg@users.noreply.github.com>
2018-02-12 07:58:01 -0600

    Add files via upload

---
+++ ZaraLab/WebSite/ZaraLab/Scripts/ScriptMain.js
@@ -0,0 +1,6 @@
+﻿window.onload = function () {
+    document.querySelector('.boton').addEventListener('click', function () {
+        document.querySelector('.container').classList.toggle('invisible');
+        this.classList.toggle('mif-chevron-right');
+    });
+}
